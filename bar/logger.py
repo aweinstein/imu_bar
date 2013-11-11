dd00e1f94352f47bc06b7a16bf218beba7eba546
aweinstein <alejandro.weinstein@gmail.com>
2013-11-11 15:46:31 -0300

    Remove unused lines.

--- bar/logger.py
+++ bar/logger.py
@@ -5,16 +5,10 @@ import matplotlib.pyplot as plt
 from scipy.io import savemat
 import serial
 
-
-#def save_data(t, p, alt, t_filt, p_filt, alt_filt):
 def save_data(times, t, p, alt):
     now = time.strftime('%Y%m%dT%H%M%S')
     file_name = 'logs/bmp085_%s.npy' % now
     mat_file_name = 'logs/bmp085_%s.mat' % now
-    ## np.save(file_name, np.array([t, p, alt, t_filt, p_filt, alt_filt]))
-    ## d = {'temperature':t, 'pressure':p, 'altitude':alt,
-    ##      'temperature_filt':t_filt, 'pressure_filt':p_filt,
-    ##      'altitude_filt':alt_filt}
 
     np.save(file_name, np.array([times, t, p, alt]))
     d = {'time':times, 'temperature':t, 'pressure':p, 'altitude':alt}
@@ -44,7 +38,6 @@ def p2alt(p, p0=1015):
 
 def plot_data(t, p, alt, t_filt=[], p_filt=[], alt_filt=[], ups=[], uts=[]):
     time = np.linspace(0, len(t)*(1/5.), len(t))
-    #f, (a1, a2, a3, a4, a5) = plt.subplots(5, sharex=True)
     _, (a1, a2, a3) = plt.subplots(3, sharex=True)
     a1.plot(time, t)
     a1.set_ylabel('Temperature [C]')
@@ -66,23 +59,12 @@ def plot_data(t, p, alt, t_filt=[], p_filt=[], alt_filt=[], ups=[], uts=[]):
         alts = np.hstack((alt, alt_filt))
         a3.set_ylim(min(alts) - 1, max(alts) + 1)
 
-    ## a4.plot(time, ups)
-    ## a4.set_ylabel('UP')
-
-    ## a5.plot(time, uts)
-    ## a5.set_ylabel('UT')
-
-    ## for a in (a1, a2, a3, a4, a5):
-    ##     a.grid()
-
 if __name__ == '__main__':
     port = '/dev/ttyACM0'
     bauds = 115200
     ser = serial.Serial(port, bauds)
     temps, press =  [], []
     times = []
-    ## temps_filt, press_filt = [], [],
-    ## ups, uts = [], []
     print 'Start logging at', time.asctime()
     start = time.time()
     i = 0
@@ -94,18 +76,12 @@ if __name__ == '__main__':
             if line[0] == '#' and line[-1] == '$':
                 values = line[1:-1].split(',')
                 if len(values) == 2:
-                    #t, t_filt, p, p_filt, up, ut = values
                     t, p = values
                     temps.append(float(t) / 10.)
                     press.append(float(p))
                     times.append(ts)
-                    ## temps_filt.append(float(t_filt) / 10.)
-                    ## press_filt.append(float(p_filt))
-                    ## ups.append(up)
-                    ## uts.append(ut)
                     i += 1
                     if i == 10:
-                        #print t, t_filt, p, p_filt
                         s = 'time: %.2f temperature: %.1f pressure: %.1f'
                         print s % ((ts -  start),
                                    float(t) / 10.,
@@ -122,9 +98,6 @@ if __name__ == '__main__':
     f = len(temps) / (end - start)
     print 'Logged %d data points with frequency %.1f' % (len(temps), f)
     altitude = p2alt(np.array(press)/100)
-    #altitude_filt = p2alt(np.array(press_filt)/100)
-    #save_data(temps, press, altitude, temps_filt, press_filt, altitude_filt)
-    #plot_data(temps, press, altitude, temps_filt, press_filt, altitude_filt, ups, uts)
     save_data(times, temps, press, altitude)
-    plot_data(temps, press, altitude)
-    plt.show()
+    ## plot_data(temps, press, altitude)
+    ## plt.show()
